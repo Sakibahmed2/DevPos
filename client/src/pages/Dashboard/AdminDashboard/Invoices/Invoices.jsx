76ba34d2528c4aa19d1c47c7e144871386046b97
unknown <mauijaloskor@gmail.com>
2024-10-09 21:04:29 +0600

    sales return and sales report done

--- client/src/pages/Dashboard/AdminDashboard/Invoices/Invoices.jsx
+++ client/src/pages/Dashboard/AdminDashboard/Invoices/Invoices.jsx
@@ -1,7 +1,276 @@
-import React from "react";
+import {
+  Box,
+  Chip,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import PaginationUi from "../../../../components/ui/PaginationUi";
+import SectionTitle from "../../../../components/ui/SectionTitle";
+
+// icons
+import searchIcon from "../../../../assets/dashboard icons/search.svg";
+
+const tableData = [
+  {
+    id: 1,
+    invoiceNo: "INV-123456",
+    customerName: "John Doe",
+    amount: 1000,
+    paid: 1000,
+    due: 0,
+    status: "Paid",
+    date: "09 Sep 2024",
+  },
+  {
+    id: 2,
+    invoiceNo: "INV-123456",
+    customerName: "John Doe",
+    amount: 1000,
+    paid: 1000,
+    due: 0,
+    status: "Unpaid",
+    date: "09 Sep 2024",
+  },
+  {
+    id: 3,
+    invoiceNo: "INV-123456",
+    customerName: "John Doe",
+    amount: 1000,
+    paid: 1000,
+    due: 0,
+    status: "Paid",
+    date: "09 Sep 2024",
+  },
+];
 
 const Invoices = () => {
-  return <div>Invoices</div>;
+  const [sortBy, setSortBy] = useState("");
+  const [page, setPage] = useState(0);
+
+  // const itemsPerPage = 3 ;
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  const columns = [
+    {
+      field: "invoiceNo",
+      headerName: "Invoice no",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.invoiceNo}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "customerName",
+      headerName: "Customer",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.customerName}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.date}</Typography>
+          </Box>
+        );
+      },
+    },
+
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">${row.amount}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "paid",
+      headerName: "Paid",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">${row.paid}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "due",
+      headerName: "Amount due",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">${row.due}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            {
+              <Chip
+                variant="outlined"
+                size="small"
+                sx={{
+                  color: row.status === "Paid" ? "primary.main" : "red",
+                  borderRadius: 1,
+                  border:
+                    row.status === "Paid"
+                      ? "1px solid lightgreen"
+                      : "1px solid red",
+
+                  px: 1,
+                }}
+                label={row.status}
+              />
+            }
+          </Box>
+        );
+      },
+    },
+  ];
+
+  const rows = tableData.map((data) => {
+    return {
+      id: data.id,
+      invoiceNo: data.invoiceNo,
+      customerName: data.customerName,
+      amount: data.amount,
+      paid: data.paid,
+      due: data.due,
+      status: data.status,
+      date: data.date,
+    };
+  });
+
+  return (
+    <Container>
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <SectionTitle
+          title={"Invoice reports"}
+          description={"Manage your invoices here"}
+        />
+      </Stack>
+
+      <Box
+        sx={{
+          mt: 5,
+          border: "1px solid lightgray",
+        }}
+      >
+        {/* search fields */}
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          sx={{
+            pt: 3,
+            px: 2,
+          }}
+        >
+          <Box
+            sx={{
+              width: "250px",
+            }}
+          >
+            <TextField
+              label="Search here"
+              fullWidth
+              slotProps={{
+                input: {
+                  endAdornment: <img src={searchIcon} />,
+                },
+              }}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              width: "170px",
+            }}
+          >
+            <FormControl fullWidth>
+              <InputLabel>Sort by date</InputLabel>
+              <Select
+                value={sortBy}
+                label="Sort by date"
+                onChange={(e) => setSortBy(e.target.value)}
+              >
+                <MenuItem value={"date"}>date</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Stack>
+
+        {/* Table */}
+        <Box
+          sx={{
+            mt: 5,
+          }}
+        >
+          <DataGrid
+            sx={{
+              border: 0,
+              borderTop: "1px solid lightgray",
+            }}
+            rows={rows}
+            columns={columns}
+            rowHeight={80}
+            hideFooter
+            checkboxSelection
+            disableRowSelectionOnClick
+          />
+        </Box>
+      </Box>
+
+      <Box>
+        <PaginationUi
+          totalItems={tableData.length}
+          currentPage={page}
+          onPageChange={handlePageChange}
+        />
+      </Box>
+    </Container>
+  );
 };
 
 export default Invoices;
--- client/src/pages/Dashboard/AdminDashboard/SalesReturn/SalesReturn.jsx
+++ client/src/pages/Dashboard/AdminDashboard/SalesReturn/SalesReturn.jsx
@@ -1,7 +1,400 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Chip,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import PaginationUi from "../../../../components/ui/PaginationUi";
+import SectionTitle from "../../../../components/ui/SectionTitle";
+
+// icons
+import deleteIcon from "../../../../assets/dashboard icons/delete-icon.svg";
+import editIcons from "../../../../assets/dashboard icons/edit-icon.svg";
+import plusIcon from "../../../../assets/dashboard icons/plusIcon.svg";
+import searchIcon from "../../../../assets/dashboard icons/search.svg";
+import EditSalesModal from "../Sales/EditSalesModal";
+import CreateWarrantyModal from "../Warranties/CreateWarrantyModal";
+import laptopImg from "../../../../assets/laptopPng.png";
+
+const tableData = [
+  {
+    id: 1,
+    productImg: laptopImg,
+    productName: "Laptop",
+    customerName: "John Doe",
+    status: "Received",
+    total: 1000,
+    paid: 1000,
+    due: 0,
+    payment: "Paid",
+    date: "09 Sep 2024",
+  },
+  {
+    id: 2,
+    productImg: laptopImg,
+    productName: "Laptop",
+    customerName: "John Doe",
+    status: "Received",
+    total: 1000,
+    paid: 1000,
+    due: 0,
+    payment: "Paid",
+    date: "09 Sep 2024",
+  },
+  {
+    id: 3,
+    productImg: laptopImg,
+    productName: "Laptop",
+    customerName: "John Doe",
+    status: "Pending",
+    total: 1000,
+    paid: 1000,
+    due: 0,
+    payment: "Unpaid",
+    date: "09 Sep 2024",
+  },
+];
 
 const SalesReturn = () => {
-  return <div>SalesReturn</div>;
+  const [sortBy, setSortBy] = useState("");
+  const [page, setPage] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [productId, setProductId] = useState(null);
+  const [createModal, setCreateModal] = useState(false);
+
+  // const itemsPerPage = 3 ;
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  const handleModal = (productId) => {
+    setOpen(true);
+    setProductId(productId);
+  };
+
+  const columns = [
+    {
+      field: "productImg",
+      headerName: "Products",
+      width: 90,
+      renderCell: ({ row }) => {
+        return (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              width: "100%",
+            }}
+          >
+            <img src={row.productImg} alt="laptop" className="h-8 w-12" />
+          </Box>
+        );
+      },
+    },
+    {
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.productName}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.date}</Typography>
+          </Box>
+        );
+      },
+    },
+
+    {
+      field: "customerName",
+      headerName: "Customer name",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.customerName}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            {
+              <Chip
+                variant="outline"
+                size="small"
+                sx={{
+                  backgroundColor:
+                    row.status === "Received" ? "primary.main" : "red",
+                  color: "white",
+                  borderRadius: 1,
+                }}
+                label={row.status}
+              />
+            }
+          </Box>
+        );
+      },
+    },
+    {
+      field: "total",
+      headerName: "Grand total",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.total}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "paid",
+      headerName: "Paid",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">{row.paid}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "due",
+      headerName: "Due",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Typography variant="p">${row.due}</Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "payment",
+      headerName: "Payment",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            {
+              <Chip
+                variant="outlined"
+                size="small"
+                sx={{
+                  color: row.payment === "Paid" ? "primary.main" : "red",
+                  borderRadius: 1,
+                  border:
+                    row.payment === "Paid"
+                      ? "1px solid lightgreen"
+                      : "1px solid red",
+                  px: 1,
+                }}
+                label={row.payment}
+              />
+            }
+          </Box>
+        );
+      },
+    },
+    {
+      field: "id",
+      headerName: "Action",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Stack
+            direction={"row"}
+            gap={1}
+            alignItems={"center"}
+            sx={{
+              height: "100%",
+              width: "100%",
+            }}
+          >
+            <Box
+              onClick={() => handleModal(row.id)}
+              component={"button"}
+              sx={{
+                border: "1px solid gray",
+                borderRadius: 1,
+                p: "5px 3px",
+              }}
+            >
+              <img src={editIcons} alt="" className="w-5 h-5" />
+            </Box>
+
+            <Box
+              component={"button"}
+              sx={{
+                border: "1px solid gray",
+                borderRadius: 1,
+                p: "5px 3px",
+              }}
+            >
+              <img src={deleteIcon} alt="" className="w-5 h-5" />
+            </Box>
+          </Stack>
+        );
+      },
+    },
+  ];
+
+  const rows = tableData.map((data) => {
+    return {
+      id: data.id,
+      productImg: data.productImg,
+      productName: data.productName,
+      customerName: data.customerName,
+      status: data.status,
+      total: data.total,
+      paid: data.paid,
+      due: data.due,
+      payment: data.payment,
+      date: data.date,
+    };
+  });
+
+  return (
+    <Container>
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <SectionTitle
+          title={"Sales return list"}
+          description={"Manage your sales return here"}
+        />
+
+        <Button
+          onClick={() => setCreateModal((prev) => !prev)}
+          startIcon={
+            <img
+              src={plusIcon}
+              alt="plus icon"
+              style={{ width: 30, height: 30 }}
+            />
+          }
+        >
+          Add new sale return
+        </Button>
+      </Stack>
+
+      <Box
+        sx={{
+          mt: 5,
+          border: "1px solid lightgray",
+        }}
+      >
+        {/* search fields */}
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          sx={{
+            pt: 3,
+            px: 2,
+          }}
+        >
+          <Box
+            sx={{
+              width: "250px",
+            }}
+          >
+            <TextField
+              label="Search here"
+              fullWidth
+              slotProps={{
+                input: {
+                  endAdornment: <img src={searchIcon} />,
+                },
+              }}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              width: "170px",
+            }}
+          >
+            <FormControl fullWidth>
+              <InputLabel>Sort by date</InputLabel>
+              <Select
+                value={sortBy}
+                label="Sort by date"
+                onChange={(e) => setSortBy(e.target.value)}
+              >
+                <MenuItem value={"date"}>date</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Stack>
+
+        {/* Table */}
+        <Box
+          sx={{
+            mt: 5,
+          }}
+        >
+          <DataGrid
+            sx={{
+              border: 0,
+              borderTop: "1px solid lightgray",
+            }}
+            rows={rows}
+            columns={columns}
+            rowHeight={80}
+            hideFooter
+            checkboxSelection
+            disableRowSelectionOnClick
+          />
+        </Box>
+      </Box>
+
+      <Box>
+        <PaginationUi
+          totalItems={tableData.length}
+          currentPage={page}
+          onPageChange={handlePageChange}
+        />
+      </Box>
+
+      {/* Edit warranty modal */}
+      <EditSalesModal open={open} setOpen={setOpen} id={productId} />
+
+      {/* Add warranty modal */}
+      <CreateWarrantyModal open={createModal} setOpen={setCreateModal} />
+    </Container>
+  );
 };
 
 export default SalesReturn;
